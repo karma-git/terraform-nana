228805d0d305527507c5f224a44df3fe2dd928a3
Andrew Horbach <andrewhorbach@gmail.com>
2021-09-12 15:53:29 +0300

    chore(6): tag <Name> for vpc,subnet

--- main.tf
+++ main.tf
@@ -5,10 +5,16 @@ provider "aws" {
 
 resource "aws_vpc" "dev-vpc" {
     cidr_block = "10.0.0.0/16"
+    tags = {
+        Name: "development"
+    }
 }
 
 resource "aws_subnet" "dev-subnet-1" {
     vpc_id = aws_vpc.dev-vpc.id
     cidr_block = "10.0.10.0/24"
     availability_zone = "us-east-2a"
+    tags = {
+        Name: "subnet-1-dev"
+    }
 }
