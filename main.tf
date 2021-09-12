5321a1a262cd7c86acaab5ea55a8002a28be70e1
Andrew Horbach <andrewhorbach@gmail.com>
2021-09-12 15:50:21 +0300

    chore(6): vpc & subnet

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+    region = "us-east-2"
+    profile = "karma-it-aws"
+}
+
+resource "aws_vpc" "dev-vpc" {
+    cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "dev-subnet-1" {
+    vpc_id = aws_vpc.dev-vpc.id
+    cidr_block = "10.0.10.0/24"
+    availability_zone = "us-east-2a"
+}
