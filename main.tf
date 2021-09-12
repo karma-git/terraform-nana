22dd3b572fe2f8b1cd733564585c16c99a865061
Andrew Horbach <andrewhorbach@gmail.com>
2021-09-12 18:34:50 +0300

    style: var assigne from : to =

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 resource "aws_vpc" "dev-vpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
-        Name: "development"
+        Name = "development"
     }
 }
 
@@ -15,7 +15,7 @@ resource "aws_subnet" "dev-subnet-1" {
     cidr_block = "10.0.10.0/24"
     availability_zone = "us-east-2a"
     tags = {
-        Name: "subnet-1-dev"
+        Name = "subnet-1-dev"
     }
 }
 
